757c2733e4ec572201a6039f1e5f1d342b20ac64
hasnarmyy <hasnarmyy@gmail.com>
2026-01-13 01:53:07 +0700

    perbaikan role admin

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -3,25 +3,23 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
 {
-    public function handle(Request $request, Closure $next)
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
     {
-        // Belum login
-        if (!Auth::check()) {
-            return redirect()->route('login');
+        if (Auth::check() && (int) Auth::user()->role_id === 1) {
+            return $next($request);
         }
 
-        $user = Auth::user();
-
-        // CAST KE INT (INI KUNCINYA)
-        if ((int) $user->role_id !== 1) {
-            abort(403, 'Unauthorized');
-        }
-
-        return $next($request);
+        abort(401, 'Unauthorized');
     }
 }

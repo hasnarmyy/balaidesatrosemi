6a5750cbd82e1e7272231b165f39da332bbdf462
hasnarmyy <hasnarmyy@gmail.com>
2026-01-13 23:34:31 +0700

    apdet

--- resources/views/layouts/landing/main.blade.php
+++ resources/views/layouts/landing/main.blade.php
@@ -8,6 +8,7 @@
     <meta name="keywords" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/quicksand.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">

9f566ea9541c2f85c2bcfd8e27e0b0fef76b604b
hasna <hasnarmyy@gmail.com>
2026-02-16 21:33:00 +0700

    commit gatau

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,10 +16,10 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && Auth::user()->role_id === 1) {
+        if (Auth::check() && (int) Auth::user()->role_id === 1) {
             return $next($request);
         }
 
-        abort(403, 'Unauthorized');
+        abort(401, 'Unauthorized');
     }
 }
